e4b1296d0bfc6e70c5ce68e4a2e14acd1522d53d
atsq <ap3994741@gmail.com>
2025-10-20 19:22:12 +0300

    aliases vs types

---
+++ lessons/12-aliases-vs-interface/pracice.ts
@@ -0,0 +1,46 @@
+// create two variants: aliases && interface
+
+// Product: price, isNew, isSale, title
+// Vehicle: wheels, year, brand
+// Car: type, model, + Product, + Vehicle
+
+// interface
+
+interface IProduct {
+	price: string;
+	isNew: boolean;
+	isSide?: boolean;
+	title: string;
+}
+
+interface IVehicle {
+	wheels: number;
+	year: string;
+	brand: string;
+}
+
+interface ICar extends IProduct, IVehicle {
+	type: string;
+	model: string;
+}
+
+// type
+
+type TProduct = {
+	price: string;
+	isNew: boolean;
+	isSide: boolean;
+	title: string;
+};
+
+type TVehicle = {
+	wheels: number;
+	year: string;
+	brand: string;
+};
+
+type TCar = {
+	type: string;
+	model: string;
+} & TProduct &
+	TVehicle;
